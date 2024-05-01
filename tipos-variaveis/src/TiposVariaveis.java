6111ab52e117a27e0a8cff26f419c6036d257750
oliveirasimplicio <jovapozao@gmail.com>
2024-04-30 23:13:36 -0300

    update

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal1 = 1;
+        short numeroCurto2 = (short) numeroNormal1;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        double VALOR_DE_PI = 3.14; //adicionando final o valor nao pode ser alterado
+
+        VALO_DE_PI = 10.7
+
     }
 }
